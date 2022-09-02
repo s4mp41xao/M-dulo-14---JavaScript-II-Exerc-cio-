4dcff556487e9b458e5390a8a612735cf978c54d
samribe <samribe@outlook.com>
2022-09-01 22:24:20 -0300

    resolucao

---
+++ Exercício/resolucao.js
@@ -0,0 +1,71 @@
+/* 10 + 15 = 25 (number);
+
+“10” * 2 = ‘102’ (string);
+
+“10” * 2 = 20 (number);
+
+“10” / 3 = 3.33 (float);
+
+“10” % 3 = 1 (number);
+
+10 + true = 11 (number);
+
+10 == ”10” = true (boolean);
+
+10 === “10” = false (boolean);
+
+10 < 11 = true (boolean);
+
+10 > 12 = false (boolean);
+
+10 <= 10.1 = false (boolean)
+
+10 > 9.99 = true (boolean)
+
+10 != “dez” = true (boolean)
+
+10 + true = 11 (number)
+
+“dez” + true = ‘deztrue’ (string)
+
+10 + false = 10 (number)
+
+10 * false = 0 (number)
+
+true + true = 2 (number)
+
+10++ = 11 (number)
+10-- = 9 (number)
+1 & 1 = 1 (number)
+1 & 0 = 0 (number)
+0 & 0 = 0 (number)
+1 & 0 = 0 (number)
+
+0 / 1 = 0 (number)
+5 + 5 == 10 = true (boolean)
+“5” + ”5” == 10 = false (boolean)
+“5” * 2 > 9 = true (boolean)
+(10 + 10) * 2 = 40 (number)
+10 + 10 * 2 = 30 (number) */
+
+var branco = "preto";
+var preto = "cinza";
+var cinza = "branco";
+var carro = "preto";
+var valor = 30000;
+var prestacao = 750;
+
+/* A) branco == "branco" = false
+
+B) branco == cinza = false
+
+C) carro === branco = true 
+
+D) var cavalo = carro == "preto" ? "cinza" : "marron" = cinza
+
+E) > var valor = 30000
+> var prestacao = 750
+> entrada = 3000
+> valor - entrada = 27000
+> prestacao * 36 = 27000
+> será 36 prestações de R$ 750 para pagar o valor do carro ou 12 prestações de R$ 2250 */
